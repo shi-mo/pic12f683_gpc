10fa2531d860ee931e8e91601d91f72ee5b91c3e
Yoshifumi Shimono <yoshifumi.shimono@gmail.com>
2012-06-06 21:20:04 +0900

    Declaring PIC hardware configuration.

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <htc.h>
 
+__CONFIG( \
+	INTIO \		/* Use internal oscillator without CLKOUT */
+	& WDTDIS \	/* Disable watch dog timer */
+	& PROTECT \	/* Protect program code */
+);
+
 int
 main(int argc, char *argv[])
 {
 	while (1) {
-		/* LOOP: Waiting for interrupt */
+		/* LOOP: Wait for interrupt */
 	}
 }
